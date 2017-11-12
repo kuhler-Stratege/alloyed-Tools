9cfedf34e4c1f8cf5f745a49c1e8930798dbe26c
kuhler-Stratege <Joelkuehle@gmail.com>
2017-11-12 20:28:12 +0100

    A wooden varnish item

---
+++ src/main/java/net/modding/items/holzlackierung/ItemKohle_eimer.java
@@ -0,0 +1,42 @@
+package net.modding.items.holzlackierung;
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+
+public class ItemKohle_eimer extends BasicItemTeer{
+
+	public ItemKohle_eimer() {
+		super("kohle_eimer");
+		maxStackSize = 3;
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		if (!worldIn.isRemote) {
+		Item item = itemStackIn.getItem();
+		if (item == registry.kohle_eimer) {
+			EntityItem droppeditem = new EntityItem(worldIn, playerIn.posX, playerIn.posY -1, playerIn.posZ, new ItemStack(Items.COAL));
+			worldIn.spawnEntityInWorld(droppeditem);
+			return super.onItemRightClick(new ItemStack(Items.BUCKET), worldIn, playerIn, hand);
+			}
+		}
+		return super.onItemRightClick(itemStackIn, worldIn, playerIn, hand);
+	}
+	
+	@Override
+	public boolean hasContainerItem() {
+		return true;
+	}
+	
+	@Override
+	public ItemStack getContainerItem(ItemStack stack) {
+		return new ItemStack(Items.BUCKET);
+	}
+}
---
+++ src/main/java/net/modding/items/holzlackierung/ItemTeer_stick.java
@@ -0,0 +1,49 @@
+package net.modding.items.holzlackierung;
+
+import java.util.List;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.translation.I18n;
+import net.minecraft.world.World;
+import net.modding.fludis.holzlackierung.Teer;
+import net.modding.util.UnluckEffekte;
+
+public class ItemTeer_stick extends BasicItemTeer{
+
+	public ItemTeer_stick() {
+		super("teer_stick");
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+		
+		tooltip.add(TextFormatting.DARK_RED + I18n.translateToLocal("text.teer.text"));
+	}
+	
+	@Override
+	public void onCreated(ItemStack stack, World worldIn, EntityPlayer playerIn) {
+		if (!worldIn.isRemote) {
+			Item item = stack.getItem();
+			if (item == registry.teer_eimer) {
+				EntityItem droppedItem = new EntityItem(worldIn, playerIn.posX, playerIn.posY -1, playerIn.posZ, new ItemStack(Items.BUCKET));
+				worldIn.spawnEntityInWorld(droppedItem);
+			}
+		}
+		super.onCreated(stack, worldIn, playerIn);
+	}
+	
+	@Override
+	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+		super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
+		UnluckEffekte.unluckItem(this, entityIn);
+	}
+}

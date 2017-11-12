eb26a67f23caa3829be60104178030094e4c001f
kuhler-Stratege <Joelkuehle@gmail.com>
2017-11-12 20:22:44 +0100

    Basic Class for all iron alloy blocks
    Basic Class for all iron alloy blocks.

---
+++ src/main/java/net/modding/blocks/eisenlegierung/BasicBlockEisen.java
@@ -0,0 +1,25 @@
+package net.modding.blocks.eisenlegierung;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.modding.main.Benennung_Registry;
+import net.modding.main.modding;
+
+public class BasicBlockEisen extends Block{
+
+	public static Benennung_Registry registry = modding.registry;
+	
+	public BasicBlockEisen(String name, Material material, int level, float harte, SoundType sound) {
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(registry.chromTab);
+		setHarvestLevel("pickaxe", level);
+		setHardness(harte);
+		setSoundType(sound);
+		//Holz, Gold: 0, Stein: 1, Eisen: 2, Diamant: 3
+	}
+}

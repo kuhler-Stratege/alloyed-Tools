3d40eb184f584c7b65cf7a07e9d827092da59019
kuhler-Stratege <Joelkuehle@gmail.com>
2017-11-12 20:23:17 +0100

    an iron alloy block
    An iron alloy block.

---
+++ src/main/java/net/modding/blocks/eisenlegierung/BlockFerro_Block.java
@@ -0,0 +1,27 @@
+package net.modding.blocks.eisenlegierung;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+
+public class BlockFerro_Block extends BasicBlockEisen{
+	
+	public BlockFerro_Block() {
+		super("ferro_block", Material.IRON, 2, 3.5F, SoundType.METAL);
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return registry.ferro_ingot;
+	}
+	
+	@Override
+	public int quantityDropped(IBlockState state, int fortune, Random random) {
+		return 9;
+	}
+
+}

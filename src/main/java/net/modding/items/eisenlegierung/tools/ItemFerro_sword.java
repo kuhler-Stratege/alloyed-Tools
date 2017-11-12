1b06ba35db6b15c0d4ef369a7afd8f1e696a8989
kuhler-Stratege <Joelkuehle@gmail.com>
2017-11-12 20:26:51 +0100

    An iron alloy tool

---
+++ src/main/java/net/modding/items/eisenlegierung/tools/ItemFerro_sword.java
@@ -0,0 +1,10 @@
+package net.modding.items.eisenlegierung.tools;
+
+import net.modding.items.basictools.BasicItemSword;
+
+public class ItemFerro_sword extends BasicItemSword{
+	
+	public ItemFerro_sword() {
+		super(registry.ferroMaterial, "ferro", registry.chromTab, 0.15F);
+	}	
+}

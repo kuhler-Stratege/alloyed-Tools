223bd6de69a0051ffa6fa69f989a246c677876a7
kuhler-Stratege <Joelkuehle@gmail.com>
2017-11-12 20:27:08 +0100

    A gold alloy tool

---
+++ src/main/java/net/modding/items/goldlegierung/tools/ItemMix_sword.java
@@ -0,0 +1,11 @@
+package net.modding.items.goldlegierung.tools;
+
+import net.modding.items.basictools.BasicItemSword;
+
+public class ItemMix_sword extends BasicItemSword{
+
+	public ItemMix_sword() {
+		super(registry.mixMaterial, "mix", registry.platinTab, 0.1F);
+
+	}	
+}

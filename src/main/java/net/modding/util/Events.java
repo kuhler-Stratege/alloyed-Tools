e22798892046f01671f56dc978dea5103aca83ed
kuhler-Stratege <Joelkuehle@gmail.com>
2017-12-28 15:20:59 +0100

    changed nothing

--- src/main/java/net/modding/util/Events.java
+++ src/main/java/net/modding/util/Events.java
@@ -18,21 +18,14 @@ import net.modding.main.modding;
 public class Events extends UnluckEffekte{
 
 	private boolean hasShownUp = false;
-	public static String DownloadURL = "https://goo.gl/YPKlEl";
 	public static Minecraft getter = Minecraft.getMinecraft();
 	public static Benennung_Registry registry = modding.registry;
 	
 	@SubscribeEvent
 	public void onClientTick(LivingUpdateEvent event) {
-		if (getter.currentScreen == null) {
-			if (event.entityLiving instanceof EntityPlayer) {
-				if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
-					if (UpdateChecker.isNewVersionAvailable() && !hasShownUp) {
-						getter.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "" + EnumChatFormatting.BOLD + StatCollector.translateToLocal("text.UpdateChecker.text") + DownloadURL + "."));
+		if (getter.currentScreen == null && event.entityLiving instanceof EntityPlayer && FMLCommonHandler.instance().getSide() == Side.CLIENT && UpdateChecker.isNewVersionAvailable() && !hasShownUp) {
+						getter.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "" + EnumChatFormatting.BOLD + StatCollector.translateToLocal("text.UpdateChecker.text") + modding.modInfo.UPDATE_URL + "."));
 						hasShownUp = true;
 					}
 				}
-			}
-		}
-	}
 }
--- src/main/java/net/modding/util/UnluckEffekte.java
+++ src/main/java/net/modding/util/UnluckEffekte.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;

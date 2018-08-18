e61957c022a0f8b0765fa6805fb3f4836283bb73
kuhler-Stratege <Joelkuehle@gmail.com>
2018-08-19 00:54:03 +0200

    Added slab with variants
    Added tared slabs with all variants, vanilla slab have do delete cheting oak planks and slabs.

---
+++ src/main/java/net/modding/blocks/holzlackierung/metadata/teerSlabs/BlockTeerSlabDouble.java
@@ -0,0 +1,14 @@
+package net.modding.blocks.holzlackierung.metadata.teerSlabs;
+
+public class BlockTeerSlabDouble extends BlockTeerSlab{
+
+	public BlockTeerSlabDouble() {
+		super("double");
+	}
+	
+	@Override
+	public boolean isDouble() {
+		return true;
+	}
+	
+}
---
+++ src/main/java/net/modding/blocks/holzlackierung/metadata/teerSlabs/BlockTeerSlabHalf.java
@@ -0,0 +1,14 @@
+package net.modding.blocks.holzlackierung.metadata.teerSlabs;
+
+public class BlockTeerSlabHalf extends BlockTeerSlab{
+
+	public BlockTeerSlabHalf() {
+		super("half");
+	}
+	
+	@Override
+	public boolean isDouble() {
+		return false;
+	}
+	
+}

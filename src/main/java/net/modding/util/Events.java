a5bc888d06890db1671810fb96dc027657ef60f4
kuhler-Stratege <Joelkuehle@gmail.com>
2018-08-19 01:02:29 +0200

    added events

--- src/main/java/net/modding/util/Events.java
+++ src/main/java/net/modding/util/Events.java
@@ -1,37 +1,94 @@
 package net.modding.util;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.translation.I18n;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.world.BlockEvent.NeighborNotifyEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
-import net.modding.main.Benennung_Registry;
+import net.modding.blocks.holzlackierung.BlockTeerFenceGate;
+import net.modding.blocks.holzlackierung.BlockTeerTreppe;
+import net.modding.blocks.holzlackierung.BlockTeerZaun;
+import net.modding.fluids.holzlackierung.BlockFluidTeer;
 import net.modding.main.modding;
+import net.modding.registry.Benennung_Registry;
+import net.modding.registry.Holzlackierung;
 
 public class Events {
 
 	private boolean hasShownUp = false;
 	public static Minecraft getter;
-	public static Benennung_Registry registry = modding.registry;
+	public Benennung_Registry registry = modding.registry;
 	
 	@SubscribeEvent
 	public void onClientTick(ClientTickEvent event) {
+		if (UpdateChecker.isNewVersionAvailable() && !hasShownUp) {
 		getter = Minecraft.getMinecraft();
 		if (getter.currentScreen == null) {
-		if (UpdateChecker.isNewVersionAvailable() && !hasShownUp) {
 			getter.thePlayer.addChatComponentMessage(new TextComponentString(TextFormatting.GOLD + "" + TextFormatting.BOLD + I18n.translateToLocal("text.UpdateChecker.text") + modding.modInfo.UPDATE_URL + "."));
 			hasShownUp = true;
 			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void registerFluidTextures(TextureStitchEvent.Pre event) {
+		TextureMap map = event.getMap();
+		
+		map.registerSprite(new ResourceLocation(modding.MODID, "blocks/" + "teer" + "_still"));
+		map.registerSprite(new ResourceLocation(modding.MODID, "blocks/" + "teer" + "_flow"));
+	}
+	
+	public void addEffekts(World world, BlockPos pos, IBlockState state) {
+		getter = Minecraft.getMinecraft();
+		if (getter.currentScreen == null) {
+		
+		}
+	}
+	
+	@SubscribeEvent
+	public void onEntityUpdate (LivingUpdateEvent event) {
+		
+	}
+	
+	@SubscribeEvent
+	public void NeighborChanged (NeighborNotifyEvent event) {
+		Holzlackierung holz = registry.holz;
+		BlockFluidTeer blockTeer = holz.blockFluidTeer;
+		World world = event.getWorld();
+		BlockPos pos = event.getPos();
+		if (event.getState().getBlock() == Blocks.WATER || event.getState().getBlock() == Blocks.FLOWING_WATER) {
+			blockTeer.ersetzen(world, pos, holz.plank_teer, Blocks.PLANKS);
+			blockTeer.ersetzen(world, pos, holz.teerHalfSlab, Blocks.WOODEN_SLAB);
+			blockTeer.ersetzen(world, pos, holz.teerDoubleSlab, Blocks.DOUBLE_WOODEN_SLAB);
+			blockTeer.ersetzen(world, pos, holz.teer_knopf, Blocks.WOODEN_BUTTON);
+			blockTeer.ersetzen(world, pos, holz.teer_pressure_plate, Blocks.WOODEN_PRESSURE_PLATE);
+			
+			if (BlockTeerZaun.getZaunArray().length > 0 && BlockTeerFenceGate.getFenceGateArray().length > 0 && BlockTeerTreppe.getTreppenArray().length > 0) {
+				try {
+					for (int i=0; i<BlockTeerZaun.getZaunArray().length; i++) {
+						blockTeer.ersetzen(world, pos, holz.teer_zaun, BlockTeerZaun.getZaunArray()[i]);
+					}
+					for (int i=0; i<BlockTeerFenceGate.getFenceGateArray().length; i++) {
+						blockTeer.ersetzen(world, pos, holz.teer_fence_gate, BlockTeerFenceGate.getFenceGateArray()[i]);
+					}
+					for (int i=0; i<BlockTeerTreppe.getTreppenArray().length; i++) {
+						blockTeer.ersetzen(world, pos, holz.teer_treppe, BlockTeerTreppe.getTreppenArray()[i]);
+						}
+					}
+			catch (NullPointerException e) {
+				e.printStackTrace();
+				}
+			} 
 		}
 	}
 }
